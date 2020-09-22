a0a5d4a76967135cd8c656752c9ce77d92828219
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-09-22 13:20:55 +0300

    Выполнила задание 3

---
+++ Задания к теме 4/Задание 3/Задание 3.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double a, b;
+	setlocale(LC_ALL, "Rus");
+	printf("Для вычесления среднего арифмитического введите значения переменных:\n");
+	scanf_s("%lf", &a);
+	scanf_s("%lf", &b);
+	printf_s("Значение среднего арифметического равно: %lf\n", (a + b) / 2);
+
+	return 0;
+
+}
