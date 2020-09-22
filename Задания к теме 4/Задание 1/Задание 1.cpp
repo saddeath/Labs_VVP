ffdecff6216a8f128493763f96bc7c48cc802720
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-09-22 11:59:53 +0300

    Загрузмла задание 1 к теме №4

---
+++ Задания к теме 4/Задание 1/Задание 1.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double a,b,S,P;
+	setlocale(LC_ALL, "Rus");
+	printf("Для вычесления периметра и площади прямоугольника введите длину и ширину:\n");
+	scanf_s("%lf",&a);
+	scanf_s("%lf",&b);
+	P = 2 * (a + b);
+	S = a * b;
+	printf_s("Периметр равен: %lf\n", P);
+	printf_s("Площадь равна: %lf\n", S);
+
+	return 0;
+
+}
