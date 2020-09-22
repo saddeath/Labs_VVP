fe24694d23f2d671cc2c013a6d6f3a16291d25ed
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-09-22 13:39:03 +0300

    Выполнила задание 4 к теме 4

---
+++ Задания к теме 4/Задание 4/Задание 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+	double a, b, sum, raz, pr, del;
+	setlocale(LC_ALL, "Rus");
+	printf("Для вычесления суммы, разности, произведения и частного их квадратов:\n");
+	scanf_s("%lf", &a);
+	scanf_s("%lf", &b);
+	sum = pow(a, 2) + pow(b, 2);
+	raz = pow(a, 2) - pow(b, 2);
+	pr = pow(a, 2) * pow(b, 2);
+	del = pow(a, 2) / pow(b, 2);
+	printf_s("Сумма равна:%lf\n", sum);
+	printf_s("Разность равна:%lf\n", raz);
+	printf_s("Произведение равно:%lf\n", pr);
+	printf_s("Частное равно:%lf\n", del);
+
+	return 0;
+}
