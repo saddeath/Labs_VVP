a4c3299f71c8b78ba6116a9ca70c90c161d91141
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-09-22 13:13:25 +0300

    Выполнила задание 2 к теме №4

---
+++ Задания к теме 4/Задание 2/Задание 2.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    double L, d, π = 3.14;
+    printf_s("Введите значение диаметра:");
+    scanf_s("%lf", &d);
+    L = π*d;
+    printf_s("Длина окружности равна:%lf\n", L);
+    return 0;
+}
