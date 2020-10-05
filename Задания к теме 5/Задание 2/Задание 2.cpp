be999fddec63ddfb1c1c347acf97b2c9afb50879
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-10-05 20:38:14 +0300

    Задание 2 выполнила

---
+++ Задания к теме 5/Задание 2/Задание 2.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	double A, B, C,AB,BC, sum;
+	setlocale(LC_ALL, "Rus");
+	printf("Для вычесления длины отрезков и их суммы введите координаты точки A:");
+	scanf_s("%lf", &A);
+	printf("Для вычесления длины отрезков и их суммы введите координаты точки B:");
+	scanf_s("%lf", &B);
+	printf("Для вычесления длины отрезков и их суммы введите координаты точки C:");
+	scanf_s("%lf", &C);
+	AB=abs(B-A);
+	BC = abs(C - B);
+	sum = AB + BC;
+	printf_s("Сумма отрезков равна:%lf\n", sum);
+
+	return 0;
+}
