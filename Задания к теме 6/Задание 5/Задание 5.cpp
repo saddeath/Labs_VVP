aea6f3804402fc8aa4d00a5767181f39704d8a63
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-10-12 01:46:38 +0300

    Выполнила задание 5

---
+++ Задания к теме 6/Задание 5/Задание 5.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>                                              
+#include <locale.h>
+#include <math.h>
+
+
+int main()
+{
+	double x, y;
+	setlocale(LC_ALL, "Rus");
+	printf_s("Выраженрие y=4(x-3)^6-7(x-3)^3+2:\n");
+	printf_s("Введите значение переменной x:");
+	scanf_s("%lf", &x);
+
+	y = 4 * pow(x - 3, 6) - 7 * pow(x - 3, 3) + 2;
+	printf_s("Итоговое значение выражения: %lf", y);
+
+	return 0;
+}
+
+
