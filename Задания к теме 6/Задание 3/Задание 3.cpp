165b75a90d68c8d878d64bd04fd55b069ade358f
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-10-12 01:25:03 +0300

    Выполнила задание 3

---
+++ Задания к теме 6/Задание 3/Задание 3.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+	double A, B, C;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите значения переменных a и b:\n");
+	scanf_s("%lf", &A);
+	scanf_s("%lf", &B);
+	scanf_s("%lf", &C);
+	A = A + C;
+	C = A - C;
+	A = A - C;
+	B = B + A;
+	A = B - A;
+	B = B - A;
+
+	printf_s("%lf\n", A);
+	printf_s("%lf\n", B);
+	printf_s("%lf\n", C);
+
+	return 0;
+
+}
