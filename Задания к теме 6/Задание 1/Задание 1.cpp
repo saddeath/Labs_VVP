71745503bf9c41f7ecc88d3d459018ec17bcf2a1
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-10-12 00:26:13 +0300

    Выполнила задание 1

---
+++ Задания к теме 6/Задание 1/Задание 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double a, b, c;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите значения переменных a и b:\n");
+	scanf_s("%lf", &a);
+	scanf_s("%lf", &b);
+	c = b;
+	b = a;
+	a = c;
+	
+	printf_s("%lf\n", a);
+	printf_s("%lf\n", b);
+
+	return 0;
+
+}
