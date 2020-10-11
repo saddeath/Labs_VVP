0e7d4c931defb42dd42b52db19886a3eac697c5e
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-10-11 22:33:51 +0300

    Выполнила задание 3

---
+++ Задания к теме 5/Задание 3/Задание 3.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	double A, B, C, AB, BC, pro ;
+	setlocale(LC_ALL, "Rus");
+	printf("Для вычесления длины отрезков и их произведения введите координаты точки A:");
+	scanf_s("%lf", &A);
+	printf("Для вычесления длины отрезков и их произведения введите координаты точки B:");
+	scanf_s("%lf", &B);
+	printf("Для вычесления длины отрезков и их произведения введите координаты точки C:");
+	scanf_s("%lf", &C);
+	if ((C > A and C < B) or (C<A and C>B)) {
+		AB = fabs(B - A);
+		BC = fabs(C - B);
+		pro = AB * BC;
+		printf_s("Произведение отрезков равно:%lf\n", pro);
+	}
+	else {
+		printf("Невозможно произвести вычесления, так как точка C не находится между A и B");
+
+	}
+
+	return 0;
+}
