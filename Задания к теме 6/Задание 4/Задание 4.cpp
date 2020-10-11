31f4bc0d198d712e38b27c200f1c984d5803f24d
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-10-12 01:38:34 +0300

    Выполнила задание 4

---
+++ Задания к теме 6/Задание 4/Задание 4.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>                                              
+#include <locale.h>
+#include <math.h>
+
+
+int main()
+{
+	double x, y;
+	setlocale(LC_ALL, "Rus");
+	printf_s("Выраженрие y = 3x^6-6x^2-7 :\n");
+	printf_s("Введите значение переменной x:");
+	scanf_s("%lf", &x);
+
+	y = 3 * pow(x, 6) - 6 * pow(x, 2) - 7;
+	printf_s("Итоговое значение выражения: %lf", y);
+
+	return 0;
+}
+
+
