c7bcca6a723541f9a3bf76a0f597ce73db7d83e8
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-10-12 23:15:03 +0300

    Выполнила задание 6

---
+++ Задания к теме 6/Задание 6/Задание 6.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>                                              
+#include <locale.h>
+#include <math.h>
+
+
+int main()
+{
+	double A, x;
+	setlocale(LC_ALL, "Rus");
+	printf_s("Введите значение переменной A:");
+	scanf_s("%lf", &A);
+
+	x = A * A;
+	x = x * pow(A,3);
+	A = x * pow(A,3);
+
+	printf_s("Итоговое значение выражения: %lf", A);
+
+	return 0;
+}
+
+
---
+++ Задания к теме 6/Задание 7/Задание 7.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>                                              
+#include <locale.h>
+#include <math.h>
+
+
+int main()
+{
+	double A, x, y;
+	setlocale(LC_ALL, "Rus");
+	printf_s("Введите значение переменной A:");
+	scanf_s("%lf", &A);
+
+	x = A * A;
+	y = pow(A, 3);
+	x = x * y;
+	y = x * x;
+	A = y * x;
+
+	printf_s("Итоговое значение выражения: %lf", A);
+
+	return 0;
+}
