c2ead5d29480164db1c1a0a02e9cce70ecfdde95
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-09 19:26:29 +0300

    Задание 4 выполнила

---
+++ Задание к теме 7/Задание 4/Задание 4.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double  V1, V2, S, T,R;
+	setlocale(LC_ALL, "Rus");
+	printf(":Введите скорость первого авто \n");
+	scanf_s("%lf", &V1);
+	printf(":Введите скорость второго авто\n");
+	scanf_s("%lf", &V2);
+	printf(":Введите расстояние между авто\n");
+	scanf_s("%lf", &S);
+	printf(":Введите время\n");
+	scanf_s("%lf", &T);
+	R = S + (V1 + V2) * T;
+	printf_s("расстояние между авто через Т время: %lf\n", R);
+
+	return 0;
+
+}
