64f0b4cadd50d3feb2c63a0a1e70144a38ef2f95
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-09 19:09:14 +0300

    Выполнила задание 2

---
+++ Задание к теме 7/Задание 2/Задание 2.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double a;
+	setlocale(LC_ALL, "Rus");
+	printf("Для вычесления значения угла в градусах, введите значение этого же угла в радианах:\n");
+	scanf_s("%lf", &a);
+	a = a * 180 / 3.14;
+	printf_s("Значение угла в радианах равно: %lf\n", a);
+
+	return 0;
+
+}
