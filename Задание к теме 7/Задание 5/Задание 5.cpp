54853eaa504b314e0c6ca480ffc0e31f2d9aab70
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-09 19:34:15 +0300

    Выполнила задание 5

---
+++ Задание к теме 7/Задание 5/Задание 5.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double  A,x,B;
+	setlocale(LC_ALL, "Rus");
+	printf(":Введите значение А \n");
+	scanf_s("%lf", &A);
+	printf(":Введите значение В\n");
+	scanf_s("%lf", &B);
+	x = -B / A;
+	printf_s("решение линейного уравнения : %lf\n", x);
+
+	return 0;
+
+}
