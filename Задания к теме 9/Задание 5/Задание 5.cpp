25f64b83ec288573796de9a30ee1b711165d48c5
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-10 19:33:11 +0300

    Выполнила задание 5

---
+++ Задания к теме 9/Задание 5/Задание 5.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+	int  A, B, C, x, y;
+	setlocale(LC_ALL, "Rus");
+	printf("Ввеите номер года:\n");
+	scanf_s("%d", &x);
+	y = ((x - 1) / 100) + 1;
+	printf("Столетие : %d\n", y);
+	
+
+	return 0;
+
+}
