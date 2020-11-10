481c4d75ce0db8648bdecc400b0642227fda5961
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-10 18:43:12 +0300

    Выполнила задание 3

---
+++ Задания к теме 8/Задание 3/Задание 3.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int A, B, x;
+	setlocale(LC_ALL, "Rus");
+	printf("Ввеите значение A:\n");
+	scanf_s("%d", &A);
+	printf("Ввеите значение B:\n");
+	scanf_s("%d", &B);
+	if (A > B) {
+		x = A % B;
+	}
+	printf("Остаток от длины А: %d\n", x);
+
+	return 0;
+
+}
