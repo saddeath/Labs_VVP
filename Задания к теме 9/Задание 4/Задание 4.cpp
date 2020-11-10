7489007dee9700de2b0977b0e89461ee329cd4ab
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-10 19:27:24 +0300

    Выполнила задание 4

---
+++ Задания к теме 9/Задание 4/Задание 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+	int  A,B,C,x, y;
+	setlocale(LC_ALL, "Rus");
+	printf("Ввеите значение A:\n");
+	scanf_s("%d", &A);
+	printf("Ввеите значение B:\n");
+	scanf_s("%d", &B);
+	printf("Ввеите значение C:\n");
+	scanf_s("%d", &C);
+    x = (A / C) * (B / C);
+	y = A * B - x * sqrt(C);
+	printf("количество квадратов: %d\n", x);
+    printf("Не занятая площадь: %d\n", y);
+
+	return 0;
+
+}
