ca55b72fcfb09ca2c991c0f79bbc531b7b4fe426
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-10 18:58:50 +0300

    Выполнила задание 5

---
+++ Задания к теме 8/Задание 5/Задание 5.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int  x, y;
+	setlocale(LC_ALL, "Rus");
+	printf("Ввеите трехзначное число:\n");
+	scanf_s("%d", &x);
+	y = (x % 100) * 10 + x / 100;
+	printf("Перевернутое трехзначное число: %d\n", y);
+
+	return 0;
+
+}
