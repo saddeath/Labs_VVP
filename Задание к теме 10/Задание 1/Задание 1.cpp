5429ee79d74d3b2014e019b5e818603fbfa60337
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-10 19:45:26 +0300

    Задание 1 выполнила

---
+++ Задание к теме 10/Задание 1/Задание 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double A,B;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите значение A:\n");
+	scanf_s("%lf", &A);
+	printf("Введите значение B:\n");
+	scanf_s("%lf", &B);
+	if ((A > 2) and (B <= 3)) {
+		printf_s("Выражение справедливо");
+	}
+	else {
+		printf_s("Выражение несправедливо");
+	}
+	
+
+	return 0;
+
+}
--- Задания к теме 9/Задание 5/Задание 5.cpp
+++ Задания к теме 9/Задание 5/Задание 5.cpp
@@ -1,6 +1,5 @@
 ﻿#include <stdio.h>
 #include <locale.h>
-#include <math.h>
 
 int main()
 {
@@ -9,8 +8,8 @@ int main()
 	printf("Ввеите номер года:\n");
 	scanf_s("%d", &x);
 	y = ((x - 1) / 100) + 1;
+
 	printf("Столетие : %d\n", y);
-	
 
 	return 0;
 
