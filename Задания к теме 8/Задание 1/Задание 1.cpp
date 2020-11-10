d01acc5e8254a1bdc222595c1e6cac38c8a77d31
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-10 18:14:01 +0300

    Выполнила задание 1

---
+++ Задания к теме 8/Задание 1/Задание 1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double a;
+	setlocale(LC_ALL, "Rus");
+	printf("Ввеите значение в байтах:\n");
+	scanf_s("%lf", &a);
+	a = a / 1024;
+	printf_s("Значение в килобайтах: %lf\n", a);
+
+	return 0;
+
+}
