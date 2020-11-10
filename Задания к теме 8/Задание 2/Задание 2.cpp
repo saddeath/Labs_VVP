2b9676f638f5247ba97739abe59fc11f98355498
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-10 18:17:54 +0300

    Выполнила задание 2

--- Задания к теме 8/Задание 2/Задание 2.cpp
+++ Задания к теме 8/Задание 2/Задание 2.cpp
@@ -1,1 +1,19 @@
-﻿
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double A,B, x;
+	setlocale(LC_ALL, "Rus");
+	printf("Ввеите значение A:\n");
+	scanf_s("%lf", &A);
+	printf("Ввеите значение B:\n");
+	scanf_s("%lf", &B);
+	if (A > B) {
+		x = A / B;
+	}
+	printf_s("Количество отрезков В, которые помещаются в А: %lf\n", x);
+
+	return 0;
+
+}
