25894d6e8babd9163d2815a5afeb44c010c1fd8e
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-10 18:50:24 +0300

    Выполнила задание 4

---
+++ Задания к теме 8/Задание 4/Задание 4.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int  x, y;
+	setlocale(LC_ALL, "Rus");
+	printf("Ввеите двузначное число:\n");
+	scanf_s("%d", &x);
+    y = (x / 10) + (x % 10) * 10;
+	printf("Перевернутое двузначное число: %d\n", y);
+
+	return 0;
+
+}
