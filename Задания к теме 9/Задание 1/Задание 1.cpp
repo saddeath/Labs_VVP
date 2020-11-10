6d9544982693ce3d0a412d9d1b11a3204af5b977
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-10 19:11:22 +0300

    Выполнила задание 1

---
+++ Задания к теме 9/Задание 1/Задание 1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int  x, y;
+	setlocale(LC_ALL, "Rus");
+	printf("Ввеите количество секунд:\n");
+	scanf_s("%d", &x);
+	y = x % 60;
+	printf("количество секунд, прошедших с начала последней минуты: %d\n", y);
+
+	return 0;
+
+}
