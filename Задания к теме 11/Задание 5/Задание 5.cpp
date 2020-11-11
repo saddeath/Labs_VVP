2263395022355efaf0edbe45538f6fd7a46a5625
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-11 18:34:39 +0300

    Выполнила задание 5

---
+++ Задания к теме 11/Задание 5/Задание 5.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+	int x;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите значение x:\n");
+	scanf_s("%d", &x);
+	
+	if (x== 0) {
+		printf("Значение числа нулевое");
+	}
+	else if ((x > 0) && (x%2==0)){
+		printf("Значение числа положительное и четное ");
+	}
+	else if ((x > 0) && (x % 2 != 0)) {
+		printf("Значение числа положительное и нечетное");
+	}
+	else if ((x < 0) && (x % 2 != 0)) {
+		printf("Значение числа отрицательное и нечетное");
+	}
+	else if ((x < 0) && (x % 2 == 0)) {
+		printf("Значение числа отрицательное и четное");
+	}
+		
+	return 0;
+}
