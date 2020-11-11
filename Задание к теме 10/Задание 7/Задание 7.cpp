fc4bb20a09a7f4377d9248ea7b2976f6d84c2643
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-11 17:37:19 +0300

    Выполнила задание 7

---
+++ Задание к теме 10/Задание 7/Задание 7.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int a, b, c;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите первую сторону :\n");
+	scanf_s("%d", &a);
+	printf("Введите вторую сторону:\n");
+	scanf_s("%d", &b);
+	printf("Введите третью сторону :\n");
+	scanf_s("%d", &c);
+
+
+	if ((a <( b + c)) &(b < (a + c))& (c < (a + b))) {
+		printf_s("Треугольник с данными сторонами существует");
+	}
+	else {
+		printf_s("Трегольник с данными сторонами не существует");
+	}
+
+	return 0;
+}
