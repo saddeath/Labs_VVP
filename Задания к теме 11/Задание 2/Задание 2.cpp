e9282248bf19ba12c9d331858f9a6cdb0e40f1f6
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-11 18:00:27 +0300

    Выполнила задание 2

---
+++ Задания к теме 11/Задание 2/Задание 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int x, y,z;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите значение x:\n");
+	scanf_s("%d", &x);
+	printf("Введите значение y:\n");
+	scanf_s("%d", &y);
+	printf("Введите значение z:\n");
+	scanf_s("%d", &z);
+	if ((x <= y) && (x <= z)) {
+		printf_s("сумма двух наибольших цичел: %d\n", y+z);
+	} 
+	else if ((y <= x) && (y <= z)) {
+		printf_s("сумма двух наибольших цичел: %d\n", x + z);
+	}
+	else if ((z <= x) && (z <= y)) {
+		printf_s("сумма двух наибольших цичел: %d\n", x + y);
+	} 
+
+	return 0;
+}
