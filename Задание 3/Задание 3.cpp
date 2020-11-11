8e8785eca4c6dbaa57be555161e21366e6c72962
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-11 17:07:34 +0300

    Выполнила задание 3

---
+++ Задание 3/Задание 3.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int x;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите число :\n");
+	scanf_s("%d", &x);
+	
+	if ((x < 100) && (x > 9) && (x % 2 == 0)) {
+		printf_s("Введенное число двузначное и четное ");
+	}
+	else {
+		printf_s("Введенное число не удовлетворяет условиям");
+	}
+
+	return 0;
+}
