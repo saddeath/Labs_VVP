db75323289f9c18558102d1c1c50fbc3e9ef36d2
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-11 17:33:46 +0300

    Выполнила задание 6

---
+++ Задание к теме 10/Задание 6/Задание 6.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+	int a,b,c;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите первую сторону :\n");
+	scanf_s("%d", &a);
+	printf("Введите вторую сторону:\n");
+	scanf_s("%d", &b); 
+	printf("Введите третью сторону :\n");
+	scanf_s("%d", &c);
+
+
+	if ((pow(a,2) == (pow(b,2) + pow(c,2))) | (pow(b,2) == (pow(a,2) + pow(c,2))) | (pow(c,2) == (pow(a,2) + pow(b,2)))) {
+		printf_s("Треугольник с данными сторонами является прямоугольным ");
+	}
+	else {
+		printf_s("Трегольник с данными сторонами не является приямоугольным");
+	}
+
+	return 0;
+}
