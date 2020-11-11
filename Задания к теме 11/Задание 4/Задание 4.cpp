bdba8b82c939754ccbcb04874b0fc7e78950209a
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-11 18:19:52 +0300

    Выполнила задание 4

---
+++ Задания к теме 11/Задание 4/Задание 4.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+	double x,y;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите значение x:\n");
+	scanf_s("%lf", &x);
+	printf("Введите значение y:\n");
+	scanf_s("%lf", &y);
+
+	if ((x > 0) && (y > 0)) {
+		printf_s("Точка лежит в 1 координатной четверти");
+	}
+	else if ((x < 0) && (y > 0)) {
+		printf_s("Точка лежит в 2 координатной четверти");
+	}
+	else if ((x < 0) && (y < 0)) {
+		printf_s("Точка лежит в 3 координатной четверти");
+	}
+	else if ((x > 0) && (y < 0)) {
+		printf_s("Точка лежит в 3 координатной четверти");
+	} 
+
+	return 0;
+}
