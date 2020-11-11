c11c4d98e0ca2ce866b22e21e680db9ac0189501
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-11 17:51:19 +0300

    Выполнила задание 1

---
+++ Задания к теме 11/Задание 1/Задание 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int A, B;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите значение A:\n");
+	scanf_s("%d", &A);
+	printf("Введите значение B:\n");
+	scanf_s("%d", &B);
+	if (A > B) {
+		B = A;
+	}else if (A < B) {
+		      A = B;
+	} else {
+		A = 0;
+		B = 0;
+	}
+	printf_s("Значение переменной А: %d\n", A);
+	printf_s("Значение переменной В: %d\n", B);
+	
+	return 0;
+}
