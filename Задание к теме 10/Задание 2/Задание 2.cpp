2d1303e9fd9e91e65a7a0e0b954dd9e8fd349119
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-11 17:02:09 +0300

    Выполнила задание 2

---
+++ Задание к теме 10/Задание 2/Задание 2.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int A, B,C;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите значение A:\n");
+	scanf_s("%d", &A);
+	printf("Введите значение B:\n");
+	scanf_s("%d", &B);
+	printf("Введите значение C:\n");
+	scanf_s("%d", &C);
+	if ((A < B) && (B <C)) {
+		printf_s("Выражение справедливо");
+	}
+	else {
+		printf_s("Выражение несправедливо");
+	}
+
+	return 0;
+}
