489ff372f2e943509f955df5d0705763ff1e0d08
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-12 13:46:46 +0300

    Изменила задание 3

--- Задания к теме 9/Задание 3/Задание 3.cpp
+++ Задания к теме 9/Задание 3/Задание 3.cpp
@@ -3,12 +3,14 @@
 
 int main()
 {
-	int  x, y;
+	int  x, y,z;
 	setlocale(LC_ALL, "Rus");
 	printf("Ввеите день:\n");
 	scanf_s("%d", &x);
-	y = x % 7 + 1;
-	printf("номер дня: %d\n", y);
+	printf("Ввеите номер днz, который будет присвоен 1 января:\n");
+	scanf_s("%d", &y);
+	z = (((x-1) + (y - 1)) % 7) + 1;
+	printf("день недели: %d\n", z);
 
 	return 0;
 
