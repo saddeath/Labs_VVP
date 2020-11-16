7fbeb31a3967c5ceff3d5efe0d3852ad3f46f7ca
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 19:09:41 +0300

    Выполнила задание 1

---
+++ Задания к теме 15/Задание 1/Задание 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <xmemory>
+
+void PowerA3(double A, double* B) {
+	*B = A * A * A;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int z;
+	for (z = 1; z <= 5; z++){
+		double A, B;
+		printf("Введите число :\n");
+		scanf_s("%lf", &A);
+		PowerA3(A, &B);
+		printf_s("A в кубе равно: %lf\n", B);
+	}
+
+	return 0;
+}
