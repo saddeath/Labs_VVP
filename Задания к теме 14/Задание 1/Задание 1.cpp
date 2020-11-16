5c8153976636ee88922ce79725bca46ac5acd152
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 15:59:25 +0300

    Выполнила задание 1

---
+++ Задания к теме 14/Задание 1/Задание 1.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double x, x1, y, y1, B, A;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите A:\n");
+	scanf_s("%lf", &A);
+	printf("Введите B:\n");
+	scanf_s("%lf", &B);
+
+	if (A < B) {
+		for (y = 1; y <= A; y++) {
+			y1 = A;
+			printf_s("значение равно: %lf\n", y1);
+		}
+		for (x = B; x >= 1; x--) {
+			x1 = B;
+			printf_s("значение равно: %lf\n", x1);
+		}
+	
+	}
+	else
+		printf_s("ошибка");
+
+	return 0;
+}
