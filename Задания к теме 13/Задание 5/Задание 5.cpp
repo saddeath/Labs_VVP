03ea0b9782fee3459fd7e1bb565bfba98cd5e4e1
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 15:26:16 +0300

    Выполнила задание 5

---
+++ Задания к теме 13/Задание 5/Задание 5.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double x, x1, y, N, A;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите N:\n");
+	scanf_s("%lf", &N);
+	printf("Введите А:\n");
+	scanf_s("%lf", &A);
+
+	x = 1;
+	x1 = 1;
+
+	for (y = 1; y <= N; y++) {
+		x1 = x1 * A * (-1);
+		x = x + x1;
+	}
+	printf_s("значение равно: %lf\n", x);
+
+	return 0;
+}
+
