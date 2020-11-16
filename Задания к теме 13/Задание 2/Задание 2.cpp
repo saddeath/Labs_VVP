3834b7a90903cab3a32d74ac8da113367f1a0731
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 15:15:20 +0300

    Выполинла задание 2

---
+++ Задания к теме 13/Задание 2/Задание 2.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double x,y, N;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите N:\n");
+	scanf_s("%lf", &N);
+
+	x = 1;
+	for (y = 1; y <= N; y++) {
+		x = x * (1 + 0.1 * y);
+	}
+	printf_s("Произведение равно: %lf\n", x);
+
+	return 0;
+}
