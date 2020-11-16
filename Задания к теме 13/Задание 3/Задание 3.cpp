0c8ca14ea02ca10012eabda0fa55bfdff9cfdd95
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 15:18:37 +0300

    Выполнила задание 3

---
+++ Задания к теме 13/Задание 3/Задание 3.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double x, y, N;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите N:\n");
+	scanf_s("%lf", &N);
+
+	x = 0;
+	for (y = 1; y <= N; y++) {
+		x = x + (2 * y - 1);
+	}
+	printf_s("Квадрат равен: %lf\n", x);
+
+	return 0;
+}
