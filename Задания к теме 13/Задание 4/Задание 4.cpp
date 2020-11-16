e0ae3c51b04630acdf7a96d987b1448666925fac
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 15:23:26 +0300

    Выполнила задание 4

---
+++ Задания к теме 13/Задание 4/Задание 4.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double x,x1, y, N,A;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите N:\n");
+	scanf_s("%lf", &N);
+	printf("Введите А:\n");
+	scanf_s("%lf", &A);
+
+	x = 1;
+	x1 = 1;
+
+	for (y = 1; y <= N; y++) {
+		x1 = x1 * A;
+		x = x + x1;
+	}
+	printf_s("сумма равна: %lf\n", x);
+
+	return 0;
+}
