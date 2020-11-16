5d6c388e31a2e7a837a25d4f118b08bed71a3ec9
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 16:16:37 +0300

    Выполнила задание 3

---
+++ Задания к теме 14/Задание 3/Задание 3.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double  N,K,x;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите число N:\n");
+	scanf_s("%lf", &N);
+
+	K = 0;
+	x = 0;
+	while (x<N) {
+		K = K + 1;
+		x = K + x;
+	}
+
+	printf_s("число равно: %lf\n", K);
+	printf_s("сумма равна: %lf\n", x);
+	return 0;
+}
