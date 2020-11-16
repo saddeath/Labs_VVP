7a61b7fab771d3d96e91f11edac762c8c99f2468
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 19:25:09 +0300

    Выполнила задание 3

---
+++ Задания к теме 15/Задание 3/Задание 3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <clocale>
+
+double RingS(double R1, double R2)
+{
+	return pow(R1, 2) * 3.14 - pow(R2, 2) * 3.14;
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double R1, R2, z;
+	for (z = 0; z < 3; z++)
+	{
+		printf("Введите радиус окружности 1 \n");
+		scanf_s("%lf", &R1);
+		printf("Ведите радиус окружности 2 \n");
+		scanf_s("%lf", &R2);
+
+		if (R1 > R2) {
+			printf_s("площадь кольца равна : %lf\n", RingS(R1, R2));
+		}
+		else {
+			printf_s("ошибка");
+		}
+
+
+	}
+	return 0;
+}
