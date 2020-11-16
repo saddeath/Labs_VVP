c42df5dc94cf8edb1bc56b322a314026fa4ee820
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 16:07:16 +0300

    Выполнила задание 2

---
+++ Задания к теме 14/Задание 2/Задание 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double  B, A;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите A:\n");
+	scanf_s("%lf", &A);
+	printf("Введите B:\n");
+	scanf_s("%lf", &B);
+    
+	while ((A - B) >= 0) {
+		A = A - B;
+	}
+
+	printf_s("Значение незанятой части А: %lf\n", A);
+	return 0;
+}
