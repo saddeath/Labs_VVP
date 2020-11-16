e3f3456baff98c7b6a4e1799235bd98703cafde6
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 16:47:23 +0300

    Выполнила задание 6

---
+++ Задания к теме 14/Задание 6/Задание 6.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int   N, F, F1, F2, K;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите число N:\n");
+	scanf_s("%d", &N);
+
+    F1 = 1;
+    F2 = 1;
+	F = 0;
+    K = 2;
+
+	while (F < N) {
+		K = K + 1;
+		F = F2 + F1;
+		F2 = F1;
+		F1 = F;
+	}
+	
+	printf_s("число К равно: %d\n", K);
+
+	return 0;
+}
