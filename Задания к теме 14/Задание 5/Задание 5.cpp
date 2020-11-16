0c531a58ac38aa6b3717bcc1644c4b68b8c2bbf0
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 16:38:44 +0300

    Выполнила задание 5

---
+++ Задания к теме 14/Задание 5/Задание 5.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int  A,B;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите число A:\n");
+	scanf_s("%d", &A);
+	printf("Введите число B:\n");
+	scanf_s("%d", &B);
+
+	while ((A != 0) and (B !=0)) {
+		if (A > B) {
+			A = A % B;
+		}
+		else
+			B = B % A;
+	}
+
+	printf_s("НОД равен: %d\n", A+B);
+
+	return 0;
+}
