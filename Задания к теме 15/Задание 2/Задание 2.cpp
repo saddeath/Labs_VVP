7a6afbc6d69607922b00e115d7444d64326e8118
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 19:17:16 +0300

    Выполнила задание 2

---
+++ Задания к теме 15/Задание 2/Задание 2.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <xmemory>
+
+int Sign(double X)
+{
+	if (X < 0) {
+		return -1;
+	}
+	if (X == 0) {
+		return 0;
+	}
+	if (X > 0) {
+		return 1;
+	}
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double A, B;
+	printf("Введите число :\n");
+	scanf_s("%lf", &A);
+	printf("Введите число :\n");
+	scanf_s("%lf", &B);
+	printf_s("Значение равно: %d\n", (Sign(A) + Sign(B)));
+	
+
+	return 0;
+}
