27e978e315103c84d8472fe6b3e608e52605f64c
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 16:25:11 +0300

    Выполнила задание 4

---
+++ Задания к теме 14/Задание 4/Задание 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double  P,S,K;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите число P:\n");
+	scanf_s("%lf", &P);
+
+	K = 0;
+	S = 1000;
+	while (S<=1100) {
+		S = S * (P / 100 + 1);
+		K = K + 1;
+	}
+
+	printf_s("количсетво месяцев: %lf\n", K);
+	printf_s("итоговый размер вклада: %lf\n", S);
+
+	return 0;
+}
