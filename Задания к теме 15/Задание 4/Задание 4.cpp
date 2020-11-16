a558186d3e21748cf56fb0fe3472391d991fab8d
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 19:48:01 +0300

    Выполнила задание 5

---
+++ Задания к теме 15/Задание 4/Задание 4.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <clocale>
+
+
+void Quarter(int x, int y)
+{
+	if ((x > 0) && (y > 0)){
+		printf("Точка лежит в 1-ой координатной плоскости \n");
+	}
+	else if ((x < 0) && (y>0)) {
+		printf("очка лежит во 2-ой координатной плоскости \n");
+	}
+	else if ((x < 0) && (y < 0)){
+		printf("ТТочка лежит в 3-ей координатной плоскости \n");
+	}
+	else if ((x > 0) && (y < 0)){
+		printf("Точка лежит в 4-ой координатной плоскости \n");
+	}
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x, y, z;
+	for (z = 0; z <= 3; z++)
+	{
+		printf("Введите значение переменной \n");
+		scanf_s("%d", &x);
+		printf("Введите значение переменной \n");
+		scanf_s("%d", &y);
+		if ((x != 0) && (y != 0)){
+			Quarter(x, y);
+		}
+		else{
+			printf_s("ошибка");
+		}
+
+
+	}
+	return 0;
+}
---
+++ Задания к теме 15/Задание 5/Задание 5.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <clocale>
+
+
+int Fact2(int N)
+{
+	int x, y;
+	if (N % 2 == 0){
+		x = 1;
+		y = 2;
+		while (N > 0){
+			x = x * y;
+			y = y + 2;
+			N = N - 2;
+		}
+		return x;
+	}
+	else{
+		x = 1;
+		y = 1;
+		while (N > 0){
+			x = x * y;
+			y = y + 2;
+			N = N - 2;
+		}
+		return x;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int N;
+	printf_s("Введите число N \n");
+	scanf_s("%d", &N);
+
+	if (N > 0){
+		printf_s("факториал N : %d", Fact2(N));
+	}
+	else{
+		printf_s("ошибка");
+	
+
+	}
+	return 0;
+}
