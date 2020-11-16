a4a45a67a9c4a5a2b2989c5de34daa1f487a75ca
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 14:24:20 +0300

    Выполнила задание 1

--- Задания к теме 12/Задание 1/Задание 1.cpp
+++ Задания к теме 12/Задание 1/Задание 1.cpp
@@ -3,25 +3,147 @@
 
 int main()
 {
-	int x,y;
-	setlocale(LC_ALL, "Rus");
-	bool days[]={ "Первое ", "Второе ", "Третье ", "Четвёртое ", "Пятое ", "Шестое", "Седьмое ", "Восьмое ", "Девятое ", "Десятое ", "Одиннадцатое ", "Двеннадацатое ",
-		"Триннадцатое ", "Четырнадцатое ", "Пятнадцатое ", "Шестнадцатое ", "Семнадцатое ","Восемнадцатое ", "Девятнадцатое ", "Двадцатое ", "Двадцать первое ", "Двадцать второе ",
-		"Двадцать третье", "Двадцать четвёртое", "Двадцать пятое", "Двадцать шестое", "Двадцать седьмое ", "Двадцать восьмое ", "Двадцать девятое ", "Тридцатое ", "Тридцать первое "};
-	bool month[]= { "января", "февраля", "марта", "апреля", "мая", "июня", "июля",
-		"августа", "сентября", "октября", "ноября", "декабря" };
+    setlocale(LC_ALL, "Rus");
+	int a,b;
+	printf("введите числа\n");
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
 
-	printf("Введите день :\n");
-	scanf_s("%d", &x);
-	printf("Введите  месяц :\n");
-	scanf_s("%d", &y);
-	printf_s("День : %i\n", days[x - 1]);
-	printf_s("Месяц: %i\n", month[y - 1]);
+	switch (a) {
+	case 1:
+		printf("первое ");
+		break;
+	case 2:
+		printf("второе ");
+		break;
+	case 3:
+		printf("третье ");
+		break;
+	case 4:
+		printf("четвёртое ");
+		break;
+	case 5:
+		printf("пятое ");
+		break;
+	case 6:
+		printf("шестое ");
+		break;
+	case 7:
+		printf("седьмое ");
+		break;
+	case 8:
+		printf("восьмое ");
+		break;
+	case 9:
+		printf("девятое ");
+		break;
+	case 10:
+		printf("десятое ");
+		break;
+	case 11:
+		printf("одиннадцатое ");
+		break;
+	case 12:
+		printf("двенадцатое ");
+		break;
+	case 13:
+		printf("тринадцатое ");
+		break;
+	case 14:
+		printf("четырнадцатое ");
+		break;
+	case 15:
+		printf("пятнадцатое ");
+		break;
+	case 16:
+		printf("шестнадцатое ");
+		break;
+	case 17:
+		printf("семнадцатое ");
+		break;
+	case 18:
+		printf("восемнадцатое ");
+		break;
+	case 19:
+		printf("девятнадцатое ");
+		break;
+	case 20:
+		printf("двадцатое ");
+		break;
+	case 21:
+		printf("двадцать первое ");
+		break;
+	case 22:
+		printf("двадцать второе ");
+		break;
+	case 23:
+		printf("двадцать третье ");
+		break;
+	case 24:
+		printf("двадцать четвёртое ");
+		break;
+	case 25:
+		printf("двадцать пятое ");
+		break;
+	case 26:
+		printf("двадцать шестое ");
+		break;
+	case 27:
+		printf("двадцать седьмое ");
+		break;
+	case 28:
+		printf("двадцать восьмое ");
+		break;
+	case 29:
+		printf("двадцать девятое ");
+		break;
+	case 30:
+		printf("тридцатое ");
+		break;
+	case 31:
+		printf("тридцать первое ");
+		break;
+	}
 
+	switch (b) {
+	case 1:
+		printf("января ");
+		break;
+	case 2:
+		printf("февраля ");
+		break;
+	case 3:
+		printf("марта ");
+		break;
+	case 4:
+		printf("апреля ");
+		break;
+	case 5:
+		printf("мая ");
+		break;
+	case 6:
+		printf("июня ");
+		break;
+	case 7:
+		printf("июля ");
+		break;
+	case 8:
+		printf("августа ");
+		break;
+	case 9:
+		printf("сентября ");
+		break;
+	case 10:
+		printf("октября ");
+		break;
+	case 11:
+		printf("ноября ");
+		break;
+	case 12:
+		printf("декабря ");
+		break;
+	}
 	return 0;
-
 }
-		
-		
 		
 		
