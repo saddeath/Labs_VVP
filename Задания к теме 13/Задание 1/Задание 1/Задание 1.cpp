2b0e8c62381ab958ad209d00f0bc1b4b30d14512
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-16 15:07:41 +0300

    Выполнила задание 1

---
+++ Задания к теме 13/Задание 1/Задание 1/Задание 1.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double x,y;
+	setlocale(LC_ALL, "Rus");
+	printf("Цена конфет за 1 кг:\n");
+	scanf_s("%lf", &x);
+
+	for (y = 0; y < 10; y++) {
+		printf_s("стоимость конфет: %lf\n", y / (10 * x));
+
+	}
+		
+	return 0;
+}
