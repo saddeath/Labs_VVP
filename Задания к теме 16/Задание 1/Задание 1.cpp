948eceeada98baa1ea8a0ca86b541c6ada7c92a7
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-29 23:53:24 +0300

    Выполнила задание 1

---
+++ Задания к теме 16/Задание 1/Задание 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int a, b, mass[10];
+	setlocale(LC_ALL, "Rus");
+	printf("Введите значение a :\n");
+	scanf_s("%d", &a);
+
+	mass[1] = 1;
+	for (b = 2; b <= a; b++){
+		mass[b]= mass[b - 1] + 2;
+	}
+
+	for (b = 2; b <= a; b++){
+		printf_s(" % i\n",mass[b]);
+	}
+
+
+	return 0;
+}
