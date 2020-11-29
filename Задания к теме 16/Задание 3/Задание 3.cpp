cafad0aeb38889c877f170b917e4868a748fc133
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-30 00:16:16 +0300

    Задание 3 выполнила

---
+++ Задания к теме 16/Задание 3/Задание 3.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int a, b, c, x, mass[100];
+	setlocale(LC_ALL, "Rus");
+	printf("Введите количество :\n");
+	scanf_s("%d", &c);
+	printf("Введите a :\n");
+	scanf_s("%d", &a);
+	printf("Введите b :\n");
+	scanf_s("%d", &b);
+
+	mass[1] = a;
+	mass[2] = b;
+	mass[3] = a + b;
+	for (x = 4; x <= c; x++) {
+		mass[x] = mass[x - 1] * 2;
+	}
+
+	for (x = 1; x <= c; x++) {
+		printf_s("% i\n", mass[x]);
+	}
+
+	return 0;
+}
