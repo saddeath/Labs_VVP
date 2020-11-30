2dd8ae4d0f0b12278fe43ddf9a2239a4d74d01c2
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-30 20:55:00 +0300

    Выполнила задание 2

---
+++ Задания к теме 17/Задание 2/Задание 2.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int  a, b, c, mass[100];
+	setlocale(LC_ALL, "Rus");
+	printf("Введите количество :\n");
+	scanf_s("%d", &a);
+	printf("Введите элементы массива :\n");
+	for (b = 1; b <= a; b++) {
+		printf("mass[%d] = ", b);
+		scanf_s("%d", &mass[b]);
+	}
+    c = mass[2] - mass[1];
+	b = 2;
+
+	while ((mass[b] - mass[b - 1]) == c) {
+		b = b + 1;
+	}
+
+	if (b == a + 1) {
+		printf_s("Массив является арифметической прогрессией: %d\n", c);
+	}
+	else
+		printf_s("Этот массив не является арифметической прогрессией:\n 0\n");
+	    
+
+	return 0;
+}
