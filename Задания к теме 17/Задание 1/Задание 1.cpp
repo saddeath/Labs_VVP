1ca52f751f2548e5f049b492b97342f37c9f5679
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-30 20:39:21 +0300

    Выполнила задание 1

---
+++ Задания к теме 17/Задание 1/Задание 1.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int  a,b,c, d, x, mass[100];
+	setlocale(LC_ALL, "Rus");
+	printf("Введите количество :\n");
+	scanf_s("%d", &a);
+	printf("Введите элементы массива :\n");
+	for (b = 1; b <= a; b++) {
+		printf("mass[%d] = ", b);
+		scanf_s("%d", &mass[b]);
+	}
+	printf("Введите c :\n");
+	scanf_s("%d", &c);
+	printf("Введите d :\n");
+	scanf_s("%d", &d);
+
+	x = 0;
+	for (b = c; b <= d; b++) {
+		x = x + mass[b];
+	}
+    
+	printf_s("% d\n", x / (d - c + 1));
+
+	return 0;
+}
