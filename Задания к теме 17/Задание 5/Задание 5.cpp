13abb4ba4877b47aaccc104fd85663a33c1833d2
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-30 22:24:48 +0300

    Выполнила задание 5

---
+++ Задания к теме 17/Задание 5/Задание 5.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int  a, b, c, mass[100];
+	setlocale(LC_ALL, "Rus");
+	printf("Введите количество :\n");
+	scanf_s("%d", &a);
+	printf("Введите элементы массива :\n");
+	for (b = 1; b <= a; b++) {
+		printf("mass[%d] = ", b);
+		scanf_s("%d", &mass[b]);
+	}
+	
+	for (b = 1; b <= a; b++) {
+		for (c = 1; c <= a; c++) {
+			if ((mass[b] == mass[c]) && (b != c) && (b < c)) {
+				printf("Номера одинаковых элементов: \n%d %d\n\n", b, c);
+			}
+		}
+	}
+
+
+	return 0;
+}
