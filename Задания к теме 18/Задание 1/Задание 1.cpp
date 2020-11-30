a9c32adaacceec7fc581f96d22d3237d2314770c
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-30 22:39:37 +0300

    Выполнила задание 1

---
+++ Задания к теме 18/Задание 1/Задание 1.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int  a, b, c, mass[100], mass2[100];
+	setlocale(LC_ALL, "Rus");
+	printf("Введите количество :\n");
+	scanf_s("%d", &a);
+	printf("Введите элементы массива :\n");
+	for (b = 1; b <= a; b++) {
+		printf("mass[%d] = ", b);
+		scanf_s("%d", &mass[b]);
+	}
+
+	printf("Введите элементы массива :\n");
+	for (b = 1; b <= a; b++) {
+		printf("mass2[%d] = ", b);
+		scanf_s("%d", &mass2[b]);
+	}
+
+	for (b = 1; b <= a; b++) {
+		mass[b] = mass[b] + mass2[b];
+		mass2[b] = mass[b] - mass2[b];
+		mass[b] = mass[b] - mass2[b];
+	}
+
+	for (b = 1; b <= a; b++) {
+		printf("Первыфй измененный массив :%d\n", mass[b]);
+	}
+
+	for (b = 1; b <= a; b++) {
+		printf("Второй измененный массив :%d\n", mass2[b]);
+	}
+
+
+
+	return 0;
+}
