149ff4e2f6ba134d420143903f6385e15a9086e8
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-12-01 13:46:38 +0300

    Выполнила задание 4

---
+++ Задания к теме 12/Задание 5/Задание 5.cpp
@@ -0,0 +1,95 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+
+int main()
+{
+
+    int x;
+    setlocale(LC_ALL, "Rus");
+    printf("Введите год:");
+    scanf_s("%d", &x);
+
+    switch ((x) % 10) {
+    case 0:
+    case 1:
+        printf("бел");
+        break;
+    case 2:
+    case 3:
+        printf("черн");
+        break;
+    case 4:
+    case 5:
+        printf("зелён");
+        break;
+    case 6:
+    case 7:
+        printf("красн");
+        break;
+    case 8:
+    case 9:
+        printf("жёлт");
+        break;
+    }
+    switch ((x + 1) % 5) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 9:
+    case 10:
+    case 11:
+        printf("ой ");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        printf("ого ");
+        break;
+    }
+
+    switch ((x + 8) % 12) {
+    case 0:
+        printf("крысы\n");
+        break;
+    case 1:
+        printf("коровы\n");
+        break;
+    case 2:
+        printf("тигра\n");
+        break;
+    case 3:
+        printf("зайца\n");
+        break;
+    case 4:
+        printf("дракона\n");
+        break;
+    case 5:
+        printf("змеи\n");
+        break;
+    case 6:
+        printf("лошади\n");
+        break;
+    case 7:
+        printf("овцы\n");
+        break;
+    case 8:
+        printf("обезьяны\n");
+        break;
+    case 9:
+        printf("курицы\n");
+        break;
+    case 10:
+        printf("собаки\n");
+        break;
+    case 11:
+        printf("свиньи\n");
+        break;
+    }
+
+
+    return 0;
+}
